98bdf56668b39f9d67458d1cd3ac1099fbb4e0af
Trevor Gale <jtrevag@gmail.com>
2021-12-20 09:56:11 -0600

    Version upgrades

--- src/BoosterRow.js
+++ src/BoosterRow.js
@@ -7,7 +7,7 @@ export default function BoosterRow({character}) {
 
     return (
     <div key={character.get("Id")}>  
-        <img src={characterCard} alt="the character card"></img>
+        <img src={characterCard} alt="the character card" heigh="321px"></img>
         <img src={itemCard1} alt="item card 1" height="321px"></img>
         <img src={itemCard2} alt="item card 2" height="321px"></img>
     </div>);
