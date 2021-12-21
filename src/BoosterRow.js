34ac912d98e975a178e3430582dac1bcefc04a65
Trevor Gale <jtrevag@gmail.com>
2021-12-21 11:10:05 -0600

    Adding dropdown and some styling

--- src/BoosterRow.js
+++ src/BoosterRow.js
@@ -1,14 +1,20 @@
-export default function BoosterRow({character}) {
-    
-    console.log(character.fields);
-    const characterCard = character.get("Character Card")[0]['url'];
-    const itemCard1 = character.get("Item Card (from Card 1)")[0]['url'];
-    const itemCard2 = character.get("Item Card (from Card 2)")[0]['url'];
+export default function BoosterRow({ character }) {
+  console.log(character.fields);
+  const characterCard = character.get("Character Card")[0]["url"];
+  const itemCard1 = character.get("Item Card (from Card 1)")[0]["url"];
+  const itemCard2 = character.get("Item Card (from Card 2)")[0]["url"];
 
-    return (
-    <div key={character.get("Id")}>  
+  return (
+    <div className="row booster-row" key={character.get("Id")}>
+      <div className="col-md-6">
         <img src={characterCard} alt="the character card" heigh="321px"></img>
+      </div>
+      <div className="col-md-3">
         <img src={itemCard1} alt="item card 1" height="321px"></img>
+      </div>
+      <div className="col-md-3">
         <img src={itemCard2} alt="item card 2" height="321px"></img>
-    </div>);
+      </div>
+    </div>
+  );
 }
