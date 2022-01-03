928dd62c7f21f51536694118fdf1f4fcdb6975d2
Trevor Gale <jtrevag@gmail.com>
2022-01-03 15:51:58 -0600

    layout changes for army manager

--- src/Manager/ArmyManagerRow.js
+++ src/Manager/ArmyManagerRow.js
@@ -1,4 +1,35 @@
-export default function ArmyManagerRow({character}){
-    console.log ("calling army manager row");
-    return <div>Army time baby</div>
+export default function ArmyManagerRow({ character }) {
+  return (
+    <div className="card-group">
+      <div className="card" Style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Hero Name</h5>
+        </div>
+      </div>
+      <div className="card" Style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Item Name 1</h5>
+        </div>
+      </div>
+      <div className="card" Style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Item Name 2</h5>
+        </div>
+      </div>
+      <div className="card" Style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Item Name 3</h5>
+        </div>
+      </div>
+      <div className="card" Style="width: 18rem;">
+          <div className="my-auto">
+            <button className="btn btn-primary">Edit Character</button>
+          </div>
+      </div>
+    </div>
+  );
 }
