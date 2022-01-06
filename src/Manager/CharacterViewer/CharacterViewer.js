73b0f86f9d2f5ed8347f50182bd8abb6f2318a86
Trevor Gale <jtrevag@gmail.com>
2022-01-06 15:33:32 -0600

    spacing is important

--- src/Manager/CharacterViewer/CharacterViewer.js
+++ src/Manager/CharacterViewer/CharacterViewer.js
@@ -9,11 +9,13 @@ export default function CharacterViewer({ miniatures, faction }) {
     <div className="row">
       {myFactionCharacters.map((character) => {
         return (
-          <div className="col-4 d-print-inline">
+          <div className="col-4 my-1 d-print-inline">
             <img key={character.get('Character Name')}
               className="img-fluid"
               src={character.get("Character Card")[0]["url"]}
               alt="Character Card"
+              width="416"
+              height="297"
             ></img>
           </div>
         );
